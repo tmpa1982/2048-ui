372070ba1cbbd9e2398108aeee9858e5912a19f8
Tran Minh Phuong Anh <tran.minh.phuong.anh@gmail.com>
2025-09-12 23:34:58 +0800

    Move board with arrow keys

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ import StartScreen from './StartScreen';
 
 function App() {
   const [game, setGame] = useState<string | null>(null);
-  const [board, setBoard] = useState(null);
+  const [board, setBoard] = useState<Board | null>(null);
 
   return (
     <div className="bg-blue-950 h-screen">
-      {game && <Game game={game} board={board} /> || <StartScreen onStart={setGame} setBoard={setBoard} />}
+      {game && <Game game={game} board={board} setBoard={setBoard} /> || <StartScreen onStart={setGame} setBoard={setBoard} />}
     </div>
   )
 }
--- src/Game.tsx
+++ src/Game.tsx
@@ -1,12 +1,52 @@
+import { useEffect } from 'react'
 import Cell from './Cell'
 import type { Board } from './types'
 
 type GameProps = {
   game: string
   board: Board
+  setBoard: (board: Board) => void
 }
 
-export default function Game({ game, board }: GameProps) {
+export default function Game({ game, board, setBoard }: GameProps) {
+  useEffect(() => {
+    const keyUp = async (e: KeyboardEvent) => {
+      console.log("Key pressed:", e.key)
+      switch (e.key) {
+        case 'ArrowLeft':
+          move('LEFT')
+          break
+        case 'ArrowRight':
+          move('RIGHT')
+          break
+        case 'ArrowDown':
+          move('DOWN')
+          break
+        case 'ArrowUp':
+          move('UP')
+          break
+      }
+    }
+
+    document.addEventListener('keyup', keyUp)
+    return () => {
+      document.removeEventListener('keyup', keyUp)
+    }
+  }, [])
+
+  async function move(direction: string) {
+    const url: string = import.meta.env.VITE_API_URL
+    const response = await fetch(`${url}/api/game/${game}/move`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ direction })
+    })
+    const data = await response.json()
+    setBoard(data.board)
+  }
+
   return (
     <div className="flex flex-col items-center justify-center">
       <p className="text-amber-300 p-8">Game ID: {game}</p>
